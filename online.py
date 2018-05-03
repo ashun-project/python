82bd86f36e7c1092dae17a918a4390d29f936ce9
ashun <ashun@gmail.com>
2018-05-03 20:05:58 +0800

    修改driver重新调用

--- online.py
+++ online.py
@@ -8,68 +8,22 @@ import pymysql
 
 conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='wangboshun', db='down_list', charset='utf8', cursorclass = pymysql.cursors.DictCursor)
 cursor = conn.cursor()
-num = 1
 computNum = 1
-path = "E:\\python\\nothing\\"
-replacePath = "E:\\python\\test\\"
 
-#* 
-    # Python用法
-        #print (driver.page_source)                   // 打印demo
-        #print (ele.get_attribute('innerHTML'))       // 打印某个元素
-        #time.sleep(num)                              // 停止等待多少时间，需要引入 import time
-        #if 'au' in a:                                // 同等于js的indexOf
-        # import pdb # pdb.set_trace()                // 同等于debugger n往下执行
-    #selenium用法    
-        # driver.close()                                  // 关闭浏览器
-        # driver.implicitly_wait(6)                       // 隐形等待6秒 (智能等待以后换成这种  需要测试)
-        # driver.page_source                              // 打印demo
-        # driver.find_element_by_partial_link_text('132') // 获取a标签的text含有123的属性
+path = "D:\\project\\python\\nothing\\"
+replacePath = "D:\\project\\python\\test\\"
 
-        # 操作没有出现在窗口的元素
-            # from selenium.webdriver.common.action_chains import ActionChains
-            # action = ActionChains(driver)
-            # action.move_to_element(a[i]).click().perform()
-
-        # Chrome驱动    
-            # Browser ='C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe' // 驱动路径
-            # os.environ["webdriver.chrome.driver"] = Browser                              // 设置环境变量
-            # 文件下载配置
-                # options = webdriver.ChromeOptions()  
-                # prefs = {'profile.default_content_settings.popups': 0, 'download.default_directory': 'E:\\python\\nothing\\'}
-                # options.add_experimental_option('prefs', prefs) 
-            # driver = webdriver.Chrome(
-                # Browser,                     // 驱动配置
-                # chrome_options=options      // 下载配置  
-            #)
-        # fireFox
-            # 文件下载
-                # profile = webdriver.FirefoxProfile()
-                # profile.set_preference('browser.download.dir', 'E:\\python\\nothing\\')
-                # profile.set_preference('browser.download.folderList', 2)
-                # profile.set_preference('browser.download.manager.showWhenStarting', False)
-                # profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-
-            # driver = webdriver.Firefox(
-                # executable_path = 'E:\\python\\geckodriver.exe',   // 驱动路径
-                # firefox_profile=profile                            // 下载配置               
-            #)    
-    # sql
-        # conn.escape_string()  // sql对HTML转义
-        # cursor.close()        // 关闭游标    
-#*
-
-#做一个等待的通用方法
 driver = ''
 def getDriver():
     global driver
     profile = webdriver.FirefoxProfile()
-    profile.set_preference('browser.download.dir', 'E:\\python\\nothing\\')
+    profile.set_preference('browser.download.dir', 'D:\\project\\python\\nothing\\')
     profile.set_preference('browser.download.folderList', 2)
     profile.set_preference('browser.download.manager.showWhenStarting', False)
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-    driver = webdriver.Firefox(executable_path = 'E:\\python\\geckodriver.exe', firefox_profile=profile)
+    driver = webdriver.Firefox(executable_path = 'D:\\project\\python\\geckodriver.exe', firefox_profile=profile)
 
+#做一个等待的通用方法
 def sleep(sec=9):
     time.sleep(sec)
 
@@ -113,14 +67,14 @@ def getOs(cont,currentTime):
         print('获取id出错---', e)
     else:
         url = ev.get_attribute('href')
-        if os.path.exists('E:\\python\\nothing'):
+        if os.path.exists('D:\\project\\python\\nothing'):
             try:
-                shutil.rmtree('E:\\python\\nothing')  
-                os.mkdir('E:\\python\\nothing')
+                shutil.rmtree('D:\\project\\python\\nothing')  
+                os.mkdir('D:\\project\\python\\nothing')
             except Exception as e:
                 print('操作文件失败---', e)   
         else:
-            os.mkdir('E:\\python\\nothing')
+            os.mkdir('D:\\project\\python\\nothing')
         print('下载----',url)    
         driver.execute_script("document.location.href=arguments[0]", url)
         sleep(3)
@@ -162,8 +116,9 @@ def getListDetail(arr):
                             getOs(cont, currentTime)
         else:
             print ('url not true')
-    # getPage()
-    #记得关闭selenium  1天后再次调用init
+    driver.quit()
+    sleep(86400)  #休息一天
+    init()
 def geUrltList ():
     try:
         result = driver.find_elements_by_css_selector(".no-b-border > a[class='s xst']")
@@ -204,16 +159,14 @@ def geUrltList ():
         else:
             getListDetail(arr)
 def getPage():
-    global num
     sleep(3)
-    url = 'http://www.s8bar.com/forum-234-'+ str(num) +'.html'
+    url = 'http://www.s8bar.com/forum-234-1.html'
     try:
         driver.execute_script("document.location.href=arguments[0]", url)
     except Exception as e:
         print('getPage------出错', e)
         getPage()
     else:
-        num = num + 1 
         driver.implicitly_wait(6)
         sleep(2)
         geUrltList()
@@ -230,8 +183,8 @@ def init():
         print('init-------出错', e)
         init()
     else:    
-        getPage()        
-if __name__ == "__main__":
+        getPage()    
+if __name__ == "__main__": 
     init()
 # getListDetail('http://www.s8bar.com/thread-9227434-1-1.html')
 # driver.close()
